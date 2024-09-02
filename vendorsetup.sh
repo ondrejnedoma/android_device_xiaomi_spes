be9522b4badd5f87161e54169e80def6073bbe63
Ondrej Nedoma <ondrejnedoma0@gmail.com>
2024-09-02 17:04:00 +0200

    spes: Add qti_kernel_headers sed to vendorsetup.sh
    qti_kernel_headers are otherwise conflicting with https://github.com/CHRISL7/kernel_xiaomi_sm6225

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,6 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Remove hardware/lineage/compat to avoid conflicts
 rm -rf hardware/lineage/compat/Android.bp
+
+# Rename conflicting qti_kernel_headers in source
+sed -i 's/"qti_kernel_headers"/"qti_kernel_headers_old"/g' vendor/aosp/build/soong/Android.bp
